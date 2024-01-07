c2988446845ed48f7d06a2976422f67700fcd732
Folasayo Owolabi <Phollyb4luv205@gmail.com>
2024-01-07 17:41:27 +0100

    Refactored codes

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,6 +15,14 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+  function load() {
+    let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayForecast);
+  }
 
   if (loaded) {
     return (
@@ -27,18 +35,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(displayForecast);
+    load();
 
     return null;
   }
